bb3b1fecdfe28efb30e577dfce3abbb7464969e2
Francisco Marques <francisco.marques@pearson.com>
2024-09-09 12:19:02 -0300

    Upload de Algoritmos e Logica

---
+++ aula1.py
@@ -0,0 +1,5 @@
+nome = input('Qual o seu nome: ')
+idade = input('Qual a sua idade: ')
+peso = input('Qual o seu peso: ')
+
+print('Seu nome é', nome + ', você tem', idade, 'anos de idade, e pesa', peso + 'kg')
