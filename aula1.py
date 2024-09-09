081d69ae5302c912e91176eb00bca18b9e1793de
fmarques91 <francisco.marques@fatec.sp.gov.br>
2024-09-09 15:26:39 -0300

    Exercicios

--- aula1.py
+++ aula1.py
@@ -4,4 +4,16 @@ peso = input('Qual o seu peso: ')
 
 print('Seu nome é', nome + ', você tem', idade, 'anos de idade, e pesa', peso + 'kg')
 
-#Teste
+op = 1
+while op != 0:
+    n1 = int(input('Digite o primeiro número da conta '))
+    n2 = int(input('Digite o segundo número da sua conta: '))
+    
+    print('Sua soma é:', n1 + n2)
+    
+    op = int(input('Deseja fazer uma nova soma? Digite o número 1 para "SIM" e 0 para "NÃO": '))
+    while True: 
+        if op == 0 or op == 1:
+            break
+        else:
+            op = int(input('Digite uma opção válida, 1 para "SIM" ou 0 para "NÃO": '))
---
+++ teste.py
@@ -0,0 +1,5 @@
+n = 1
+
+while n != 3:
+    n = int(input('Digite um valor: '))
+print('Fim')
