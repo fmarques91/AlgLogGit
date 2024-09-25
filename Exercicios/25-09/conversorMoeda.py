cbf67f94b4c443b3ce09b2835bade5ff9dee456f
fmarques91 <francisco.marques@fatec.sp.gov.br>
2024-09-25 19:43:43 -0300

    Saída de moeda

--- Exercicios/25-09/conversorMoeda.py
+++ Exercicios/25-09/conversorMoeda.py
@@ -1,3 +1,3 @@
 real = float(input('Digite o valor que você possui: R$'))
 
-print('Com R${} você pode comprar US${:2f}'.format(real, real / 5.43))
+print('Com R${:.2f} você pode comprar US${:.2f}'.format(real, real / 5.43))
