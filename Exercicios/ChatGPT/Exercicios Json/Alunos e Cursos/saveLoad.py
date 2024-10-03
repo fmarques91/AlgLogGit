f12d8e80b41a5dfcc2e14e606999d2ad9347c7f0
fmarques91 <francisco.marques@fatec.sp.gov.br>
2024-10-03 14:25:04 -0300

    Atualização

--- Exercicios/ChatGPT/Exercicios Json/Alunos e Cursos/saveLoad.py
+++ Exercicios/ChatGPT/Exercicios Json/Alunos e Cursos/saveLoad.py
@@ -3,7 +3,7 @@ from aluno import Aluno
 from curso import Curso
 
 def dict_curso(cursos):
-    return  [Curso(c['Curso'], c['nota']) for c in cursos]
+    return  [Curso(c['Curso'], c['nota']) for c in cursos] # Retorna uma lista com os objetos Curso
 
 def saveAlunos(bdAlunos, arquivo):
     with open(arquivo, 'w', encoding='utf-8') as f:
@@ -13,9 +13,9 @@ def loadAlunos(arquivo):
     try:
         with open(arquivo, 'r', encoding='utf-8') as f: # Abre o arquivo em formato de leitura e aceita carcacteres especiais
             bdAlunos_json = json.load(f)  # Salva o arquivo json em uma variável
-            alunos = [Aluno(a['nome'], a['idade'], dict_curso(a['curso'])) for a in bdAlunos_json]
+            alunos = [Aluno(a['nome'], a['idade'], dict_curso(a['curso'])) for a in bdAlunos_json] # Adiciona os arquivos em um objeto Aluno
             
             return alunos
     except FileNotFoundError:
-        print('Não Carregou e retornou uma lista vazia!')
+        print('Arquivo não encontrado!')
         return []
